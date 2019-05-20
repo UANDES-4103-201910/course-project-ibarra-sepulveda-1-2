2ef05acdcdcea383f909d683d552b1e5b229f510
JavierIbarra <jaibarra1@miuandes.cl>
2019-05-19 21:45:27 -0400

    Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,4 @@ class ApplicationController < ActionController::Base
 	def index
 
   end
-
-  def current_user
-    #complete this method
-    authentification = cookies["authentification"]
-    return authentification
-  end
-
-  def is_user_logged_in?
-	  #complete this method
-    logged_in = false
-    if current_user != nil
-      logged_in = true
-    end
-	  if logged_in then true else redirect_to root_path end 
-    end
 end
